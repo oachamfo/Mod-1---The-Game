9be80f6c3fb3a5cfe4ece390800f6ce28ae483d7
Owusuduah Achamfour <owusuachamfour@gmail.com>
2023-08-13 17:05:20 -0400

    Fixed a major bug: previously, functionality to click was enabled even after a player striked out. Thus, messing with the score. This bug has been fixed.

--- check-if-time-for-next-player.js
+++ check-if-time-for-next-player.js
@@ -1,5 +1,16 @@
 function checkIfTimeForNextPlayer(){
     if(numberOfStrikes==maxNumOfStrikes || numberOfCardClicks == numberOfCards){       
+        
+        //disable click funtionality if player strikes out
+        //player can no longer click on remaining cards
+        if (numberOfStrikes==maxNumOfStrikes){
+            let disableCards = gridContentArea.querySelectorAll('.flash-card')
+            length = disableCards.length;
+            for (i=0; i<length; i++){
+                disableCards[i].style.pointerEvents = "none";
+            }
+        }
+
         nextPlayer();
     }else{
         return;
