ef8cb43588173fbf17cbb7868dafada5a57b215b
Owusuduah Achamfour <owusuachamfour@gmail.com>
2023-08-14 07:13:42 -0400

    made changes to provideStats() by adding conditionals.

--- provide-stats.js
+++ provide-stats.js
@@ -1,5 +1,10 @@
-//provide game stats, which will go in #right-nav div
 function provideStats(){
+//provide game stats, which will go in #right-nav div
+    
+    //if statement added to make sure a roundNumber that
+    //is larger than maxNumOfRounds will not be displayed
+    //this will be the stats displayed while game has not ended
+    if(roundNumber<=maxNumOfRounds){
     rightDiv.innerHTML =""; //clear rightDiv
     let childHeading = document.createElement('h2');
     childHeading.innerText = "Player: "+playersArray[currentPlayerIndex].name;
@@ -9,10 +14,11 @@ function provideStats(){
     childHeading4.innerText = "Score: "+playersArray[currentPlayerIndex].score;
     rightDiv.appendChild(childHeading4);
 
-
+  
     let childHeading1 = document.createElement('h2');
     childHeading1.innerText = "Round: "+roundNumber;
     rightDiv.appendChild(childHeading1);
+    
 
     let childHeading2 = document.createElement('h2');
     childHeading2.innerText = "Strikes: "+numberOfStrikes+"/"+ maxNumOfStrikes;
@@ -21,5 +27,5 @@ function provideStats(){
     let childHeading3 = document.createElement('h2');
     childHeading3.innerText = outcomeMessageOfLatestClickPair;
     rightDiv.appendChild(childHeading3);
-  
+    } 
 }
--- set-players-array.js
+++ set-players-array.js
@@ -1,8 +1,10 @@
 function setPlayersArray(){
+    //player 1
     const player1 = new HumanPlayer();
     player1.name = "Player 1";
     player1.score = 0;
 
+    //player 2
     const player2 = new HumanPlayer();
     player2.name = "Player 2";
     player2.score = 0;
